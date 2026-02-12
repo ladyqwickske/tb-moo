d796a6560aab9431035d9ad901d32a96cca9faab
Lady Q <lady.qwickske@gmail.com>
2026-02-12 11:49:39 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Set this to your deployed Google Apps Script Web App URL
+// Example: https://script.google.com/macros/s/AKfycb.../exec
+window.GAS_WEB_APP_URL = 'https://moo-chests.lady-qwickske.workers.dev/';
